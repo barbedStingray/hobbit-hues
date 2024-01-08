fe1981bafad77164326ca4e01a1cfedc6784419e
Ben Rehmann <brehmann@me.com>
2024-01-08 17:20:16 -0600

    refactor projectitem page

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,50 +1,21 @@
 
+// IMPORTS
+// middleware
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { motion as m } from 'framer-motion';
-
-
+// css
 import './ProjectItem.css';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
+
+
 function ProjectItem(props) {
-    // Using hooks we're creating local state for a "heading" variable with
-    // a default value of 'Functional Component'
-    const store = useSelector((store) => store);
-    const [heading, setHeading] = useState('Functional Component');
 
+    // middleware variables
     const history = useHistory();
-    const dispatch = useDispatch();
 
-
-
-    function projectDetails(project) {
-
-        console.log(`go to project details`);
-        console.log(`project:`, project);
-        console.log(`URL id:`, props.project.id);
-
-        history.push(`/details/${props.project.id}`);
-
-    }
-
-    // const container = {
-    //     hidden: { opacity: 0 },
-    //     visible: {
-    //         opacity: 1,
-    //         // scale: 1,
-    //         transition: {
-    //             // duration: 2,
-    //             delayChildren: 1,
-    //             staggerChildren: 0.5
-    //         }
-    //     }
-    // };
-
-
+    // custom motion variable
     const singleProjectItem = {
         hidden: { y: 20, opacity: 0 },
         visible: {
@@ -53,38 +24,36 @@ function ProjectItem(props) {
         }
     };
 
+    // takes user to project details page
+    function projectDetails(project) {
+        history.push(`/details/${props.project.id}`);
+    }
+
 
 
     return (
 
-            <m.div
-                key={'sigleMotionProject'}
-                className="singleProjectItem"
-                variants={singleProjectItem}
+        <m.div
+            key={'sigleMotionProject'}
+            className="singleProjectItem"
+            variants={singleProjectItem}
+            id='single-project'
+            onClick={() => projectDetails(props.project)}>
 
-                id='single-project'
-                onClick={() => projectDetails(props.project)}>
+            <div id='project-photo'>
+                <img src={props.project.picture} alt="No Photo Uploaded" className='photo-project' />
+            </div>
 
-                <div id='project-photo'>
-                    <img src={props.project.picture} alt="No Photo Uploaded" className='photo-project' />
+            <div id='project-words'>
+                <div id='project-model'>
+                    <h3>{props.project.model}</h3>
                 </div>
 
-                <div id='project-words'>
-                    <div id='project-model'>
-                        <h3>{props.project.model}</h3>
-                    </div>
-
-                    <div id='project-description'>
-                        <p>{props.project.description}</p>
-                    </div>
+                <div id='project-description'>
+                    <p>{props.project.description}</p>
                 </div>
-
-            </m.div>
-
-
-
-
-
+            </div>
+        </m.div>
     );
 }
 
