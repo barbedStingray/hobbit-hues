155abf173722fb4856a7ec91442ef432062119da
Ben Rehmann <brehmann@me.com>
2024-01-08 17:27:23 -0600

    organized projects page

--- src/components/ProjectsPage/ProjectsPage.jsx
+++ src/components/ProjectsPage/ProjectsPage.jsx
@@ -1,34 +1,41 @@
 
+// IMPORTS
+// middleware
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { AnimatePresence, motion as m } from 'framer-motion';
-
-
-
-
-
+import { motion as m } from 'framer-motion';
+// components
 import ProjectItem from '../ProjectItem/ProjectItem.jsx';
-// project item import
-
+// css
 import './ProjectsPage.css';
 
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
+
 function ProjectsPage(props) {
-    // Using hooks we're creating local state for a "heading" variable with
-    // a default value of 'Functional Component'
-    const store = useSelector((store) => store);
-    const userProjects = useSelector((store) => store.userProjects);
+
+    // middleware variables
     const dispatch = useDispatch();
 
-    const [heading, setHeading] = useState('Projects');
+    // redux variables
+    const userProjects = useSelector((store) => store.userProjects);
 
-    // load projects upon page refresh
+    // variables
+    const [heading, setHeading] = useState('Projects');
+    // custom motion variable
+    const container = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                delayChildren: 0.15,
+                staggerChildren: 0.05
+            }
+        }
+    };
+
+
+    // page refresh
     function setProjectList() {
-        console.log(`setting projects`);
-        // todo dispatch action
         dispatch({ type: 'FETCH_PROJECTS' });
     }
     useEffect(() => {
@@ -37,56 +44,35 @@ function ProjectsPage(props) {
 
 
 
-    const container = {
-        hidden: { opacity: 0 },
-        visible: {
-            opacity: 1,
-            // scale: 1,
-            transition: {
-                // duration: 2,
-                delayChildren: 0.15,
-                staggerChildren: 0.05
-            }
-        }
-    };
-
-
-
 
 
     return (
 
-        <m.div 
-        key={'createMotionProjectsPage'}
-
-        className="container"
-        variants={container}
-        initial="hidden"
-        transition={{ duration: 0.75, ease: 'easeOut' }}
-        animate="visible"
-        exit={{ 
-            opacity: 0,
-            transition: { duration: 0.5 }
-        }}
-
-        id='projects-page'>
-
-
+        <m.div
+            key={'createMotionProjectsPage'}
+            className="container"
+            variants={container}
+            initial="hidden"
+            transition={{ duration: 0.75, ease: 'easeOut' }}
+            animate="visible"
+            exit={{
+                opacity: 0,
+                transition: { duration: 0.5 }
+            }}
+            id='projects-page'>
 
             <div id='project-heading'>
                 <h2>{heading}</h2>
             </div>
 
             <div id='projects-display'>
-                {
-                    userProjects.map((project) => 
+                {userProjects.map((project) =>
                     (<ProjectItem
                         key={project.id}
                         project={project}
                     />))
                 }
             </div>
-
         </m.div>
 
     );
