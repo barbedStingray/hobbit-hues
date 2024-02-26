75ed678964dd9adfa4d694417918c326c06aee67
Ben Rehmann <brehmann@me.com>
2024-02-26 16:08:38 -0600

    logout button pushes users to home, ready for presenting

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  function logoutAndReturn() {
+    dispatch({ type: 'LOGOUT' });
+    history.push('/home');
+    console.log(`sent to home`);
+  }
+
+
   return (
     <button
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
       className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
+      onClick={logoutAndReturn}
     >
       Log Out
     </button>
