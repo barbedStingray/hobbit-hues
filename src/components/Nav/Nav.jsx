73347fee18bed1c4eafbd8927092e5a2106be4c8
Ben Rehmann <brehmann@me.com>
2024-05-25 10:43:13 -0500

    updated header

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,52 +13,59 @@ import './Nav.css';
 
 function Nav() {
 
-  // redux variable
   const user = useSelector((store) => store.user);
+
+  const loggedInLinks = [
+    {
+      name: 'Colors',
+      path: '/user'
+    },
+    {
+      name: 'Projects',
+      path: '/projects'
+    },
+    {
+      name: 'Community',
+      path: '/community'
+    },
+    {
+      name: 'Tips',
+      path: '/info'
+    },
+  ];
 
 
   return (
-    <div className="nav">
+    <div className="hobbitHeader">
+
       <Link to="/home">
-        <h3 className="nav-title ringbearer">Hobbit Hues</h3>
+        <p className="headerTitle">Hobbit Hues</p>
       </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/home">
-            Login / Register
-          </Link>
-        )}
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
+      <div className='navBar'>
+        {user.id ? (
           <>
-            <Link className="navLink" to="/user">
-              Colors
+            {/* LOGGED in, show these links */}
+            {loggedInLinks.map((link) => (
+              <Link className='hobbitLink' to={link.path}>
+                {link.name}
+              </Link>
+            ))}
+            <LogOutButton className="hobbitLink" />
+          </>
+        ) : (
+          <>
+            {/* NOT logged in, show these links */}
+            <Link className="hobbitLink" to="/home">
+              Login / Register
             </Link>
-
-            <Link className="navLink" to="/projects">
-              Projects
+            <Link className="hobbitLink" to="/about">
+              About
             </Link>
-
-            <Link className="navLink" to="/community">
-              Community
-            </Link>
-
-            {/* <Link className="navLink" to="/info">
-              Info Page
-            </Link> */}
-
-            <LogOutButton className="navLink" />
           </>
         )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
       </div>
-    </div>
+    </div >
   );
 }
 
